b5f9003f2a7f5ce4d1f822eea0d0bf8db436b22a
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2024-08-28 09:13:16 +0500

    Create q2.py

---
+++ lab2/q2.py
@@ -0,0 +1,8 @@
+##a Python function named calculate_area that calculates the area of a rectangle. The
+##function should take two parameters, length and width, and return the area.
+
+def area(length,width):
+
+  print("area is: " , length*width)
+
+area(2,3)
