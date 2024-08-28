3983a25caead32f1b2704dbe2a7938725e7b9b32
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2024-08-28 10:13:33 +0500

    Create q1.py

---
+++ lab2/q1.py
@@ -0,0 +1,27 @@
+def area_trap(b1,b2,h):
+  area= 0.5*(b1+b2)*hreturn area
+
+def area_parallelogram(base,height):
+  area = base*height
+  return area
+
+def area_cylinder(radi,height):
+  area = 2*3.142*radi*(radi+height)
+  return area
+
+def vol_cylinder(radi,height):
+  vol = 3.142*radius**2*height
+  return vol
+
+print("Area of trap is: " , area_trap(2,2,3))
+
+print("Area of para is: " , area_parallelogram(3,3))
+
+
+print("Area of cylinder is: " , area_cylinder(2,2))
+
+
+print("Vol of trap is: " , vol_cylinder(2,3))
+
+
+
