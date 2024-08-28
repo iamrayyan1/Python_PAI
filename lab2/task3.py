75700027685338564c6a764ab85e54cc763604ac
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2024-08-28 09:31:14 +0500

    Create task3.py

---
+++ lab2/task3.py
@@ -0,0 +1,7 @@
+# unknown number of inputs and you have to fid max value
+def find_max(*numbers):
+
+    return max(numbers) if numbers else None
+ 
+print(find_max(2, 7, 9)) 
+print(find_max(1, 3, 5))  
