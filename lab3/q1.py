c006c24cfb3300eb3a1367d3a3105de7b4486f4f
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2024-09-07 21:47:37 +0500

    Update q1.py

--- lab3/q1.py
+++ lab3/q1.py
@@ -1,12 +1,18 @@
+# Write a Python program that reads a text file, counts the number of characters in it, and
+# prints the total word count. Handle all possible exceptions as well.
+
+# Name: Rija Ali
+# ID: 23k-0057
+
 def char_word_count(filename):
     try:
         with open(filename) as file:
             file_content = file.read()
-            file_content = file_content.replace(' ' , '')
-            char_count = len(file_content)
-            word_count = len(file_content.split())
-            print(f"Total number of characters: {char_count}")
+            word_count = len(file_content.split()) #.split() method in Python is used to split a string into a list of substrings 
+            char_count = len(file_content.replace(' ' , '')) #.replace(' ' , '') to replace white-space with no space to count characters
+            
             print(f"Total number of words: {word_count}")
+            print(f"Total number of characters: {char_count}")
 
     except FileNotFoundError:
         print(f"Error: The file '{filename}' was not found.")
@@ -17,6 +23,5 @@ def char_word_count(filename):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-# Example usage:
 filename = 'q1.txt'
 char_word_count(filename)
