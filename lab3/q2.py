08380c53c2ec8f9cd1379f0ec4650379b3468441
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2024-09-07 21:56:33 +0500

    Update q2.py

--- lab3/q2.py
+++ lab3/q2.py
@@ -1,16 +1,24 @@
+# Create a program that reads a text file, searches for a specified word or phrase, and replaces
+# all occurrences with another word or phrase. Write the modified content back to the file.
+# Handle all possible exceptions as well.
+
+# Name: Rija Ali
+# ID: 23k-0057
+
 def replace(filename, search, new):
     try:
         # Read the content of the file
         with open(filename, 'r') as file:
             file_content = file.read()
 
-        modified_content = file_content.replace(search, new)
+        modified_content = file_content.replace(search, new) #.replace() to replace old word with new word
 
-        with open(filename, 'w') as file:
-            file.write(modified_content)
+        with open(filename, 'w') as file: #opening file as w to write in a file
+            file.write(modified_content) 
 
-        print(f"All occurrences of '{search}' have been replaced with '{new}'.")
-
+        print(f"All words containing '{search}' have been replaced with '{new}'.")
+    
+    #exception handling
     except FileNotFoundError:
         print(f"Error: The file '{filename}' was not found.")
 
@@ -21,9 +29,8 @@ def replace(filename, search, new):
         print(f"An unexpected error occurred: {e}")
 
 
-# Example usage
-filename = 'q2.txt'  # Replace with your file path
-search = 'rija'  # Replace with the phrase you want to search for
-new = 'world'  # Replace with the phrase you want to use as replacement
+filename = 'q2.txt' 
+search = 'rija'  
+new = 'world'  
 
 replace(filename, search, new)
