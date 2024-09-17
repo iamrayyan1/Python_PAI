f44a62d33f29b26d01e6262b4dc2b7b1040e1db6
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2024-09-18 03:17:21 +0500

    Update q6.py

--- lab4/q6.py
+++ lab4/q6.py
@@ -2,14 +2,30 @@
 # date_of_opening and customer_name, and methods like deposit, withdraw, and
 # check_balance.
 
-class BankAccount:
-  def __init__(self):
-    self.customer_name = ""
-    self.date_of_opening = " "
-    self.account_number = " "
-    self.balance = 0.0
+class bankAccount:
+    def __init__(self,acc_num,date_opening,name,balance=0):
+        self.acc_num = acc_num
+        self.balance = balance
+        self.date_opening = date_opening
+        self.name = name
+        
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"the ammount {amount} was deposited\nnew amount is {self.balance}")
+        
+    def withdraw(self, amount):
+        if amount < self.balance:
+            self.balance -= amount
+            print(f"the amount {amount} was withdrawn")
+        else:
+            print("Insufficient balance")
+            
+    def check_balance(self):
+        print(f"account number {self.acc_num} has balance {self.balance}")
+        
 
-  def getData(self):
-    self.customer_name = input("Enter the customer's name: ")
-    self.date_of_opening = input("ENter the date of openng: ")
-    self.account_number = input()
+bank = bankAccount(101,"7-10-1999","ahmed",1789060)
+
+bank.deposit(55600)
+bank.withdraw(250000)
+bank.check_balance()
